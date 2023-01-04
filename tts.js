8df1b40ff9a87fb86fe8b4b5b489c8fa6304b7a6
Martin Randa <martin.randa@proton.me>
2023-01-04 19:07:05 +0100

    Change choice of default voice

--- tts.js
+++ tts.js
@@ -4,24 +4,27 @@ let voices = [0]
 'speechSynthesis' in window ? console.log("Web Speech API supported.") : console.log("Web Speech API not supported.")
 
 window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices().filter(function (voice) { return voice.lang == 'en-US' || voice.lang == 'cs-CZ'; })
-  let voiceSelect = document.querySelector("#voices-menu")
-  voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)))
+    voices = window.speechSynthesis.getVoices().filter(function(voice) {
+        return voice.lang == 'en-US' || voice.lang == 'cs-CZ';
+    })
+    let voiceSelect = document.querySelector("#voices-menu")
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)))
 };
 
 function speak() {
-  utter.text = document.getElementById('text').value
-  // This works but it's not good
-  if (document.querySelector("#voices-menu").value == 0) {
-    // Microsoft Jenny is number 11 if you filter 'en-US' and 'cs-CZ' in MS Edge
-    utter.voice = voices[11]
-  } else {
-    utter.voice = voices[document.querySelector("#voices-menu").value]
-  }
-  window.speechSynthesis.speak(utter)
+    utter.text = document.getElementById('text').value
+    // This works but you can't select voice 0
+    if (document.querySelector("#voices-menu").value == 0) {
+        utter.voice = voices.filter(function(voice) {
+            return voice.name == 'Microsoft Jenny Online (Natural) - English (United States)'
+        })[0]
+    } else {
+        utter.voice = voices[document.querySelector("#voices-menu").value]
+    }
+    window.speechSynthesis.speak(utter)
 }
 
 function cancel() {
-  utter.text = document.getElementById('text').value
-  window.speechSynthesis.cancel()
+    utter.text = document.getElementById('text').value
+    window.speechSynthesis.cancel()
 }
